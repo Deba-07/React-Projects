4dc57c97ef0bcb7e224c5c974a847ab13b5d6a61
Deba-07 <dynamicdeba07@gmail.com>
2024-06-26 09:00:37 +0530

    react project

--- 12megaBlogProject/src/pages/Home.jsx
+++ 12megaBlogProject/src/pages/Home.jsx
@@ -1,16 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from 'react'
 import appwriteService from "../appwrite/config";
-import { Container, PostCard } from "../components";
+import {Container, PostCard} from '../components'
 
 function Home() {
     const [posts, setPosts] = useState([])
+
     useEffect(() => {
-        appwriteService.getPosts().then((posts) => {
+        appwriteService.getPost().then((posts) => {
             if (posts) {
                 setPosts(posts.documents)
             }
         })
     }, [])
+  
     if (posts.length === 0) {
         return (
             <div className="w-full py-8 mt-4 text-center">
@@ -27,9 +29,9 @@ function Home() {
         )
     }
     return (
-        <div className="w-full py-8">
+        <div className='w-full py-8'>
             <Container>
-                <div className="flex flex-wrap">
+                <div className='flex flex-wrap'>
                     {posts.map((post) => (
                         <div key={post.$id} className='p-2 w-1/4'>
                             <PostCard {...post} />
